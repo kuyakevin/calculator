79a7cd6a2388b203f5fe6c9820bb3fbd0f2e4f13
kuyakevin <kevin_chaney@icloud.com>
2024-11-21 12:23:13 -0800

    Final calculator project.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function add (a, b) {
-    return a + b; 
+function appendToDisplay(value){
+    const display = document.getElementById('display');
+    display.value += value;
 }
 
-function subtract (a, b) {
-    return a - b;
+function clearDisplay(){
+    const display = document.getElementById('display');
+    display.value = '';
 }
 
-function multiply (a, b) {
-    return a * b;
+function calculateResult(){
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
 }
-
-function divide (a, b) {
-    return a / b;
-}
-
